33e0f9b4c8904e1dfd24a7e50288a9e5f2010e03
jiHyeon916 <ljhee916@gmail.com>
2023-06-23 17:50:19 +0900

    Update AdminController.java

--- swithMeProject/src/main/java/com/kh/swithme/admin/controller/AdminController.java
+++ swithMeProject/src/main/java/com/kh/swithme/admin/controller/AdminController.java
@@ -162,14 +162,8 @@ public class AdminController {
    @ResponseBody
    @RequestMapping(value="memberDetailBoardList.ad", produces="application/json; charset=UTF-8")
    public String memberDetailBoard(String memberId) {
-	  
-	   //System.out.println(memberId);
 	   
 	   ArrayList<Board> list = adminService.memberDetailBoard(memberId);
-	   //ArrayList<Band> Slist = adminService.memberDetailBand(memberId);
-	   
-	   System.out.println(list);
-	   
 	   return new Gson().toJson(list);
    }
    
@@ -179,8 +173,6 @@ public class AdminController {
   public String memberBandList(String memberId) {
 	  
 	  ArrayList<Band> list = adminService.memberDetailBand(memberId);
-	  System.out.println(list);
-	  
 	  return new Gson().toJson(list);
 		
   }
